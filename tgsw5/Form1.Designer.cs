f0c72bb2088491ecd028d449b5327b0b26c90c70
jessiline <jimanuela@student.ciputra.ac.id>
2022-03-21 08:29:18 +0700

    Add project files.

---
+++ tgsw5/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace tgsw5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelData = new System.Windows.Forms.Label();
+            this.textBoxData = new System.Windows.Forms.TextBox();
+            this.labelKoleksi = new System.Windows.Forms.Label();
+            this.listBoxData = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelSetting = new System.Windows.Forms.Label();
+            this.checkBoxAktif = new System.Windows.Forms.CheckBox();
+            this.radioButtonMerah = new System.Windows.Forms.RadioButton();
+            this.radioButtonBiru = new System.Windows.Forms.RadioButton();
+            this.labelOut = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelData
+            // 
+            this.labelData.AutoSize = true;
+            this.labelData.Location = new System.Drawing.Point(48, 44);
+            this.labelData.Name = "labelData";
+            this.labelData.Size = new System.Drawing.Size(53, 25);
+            this.labelData.TabIndex = 0;
+            this.labelData.Text = "Data:";
+            // 
+            // textBoxData
+            // 
+            this.textBoxData.Location = new System.Drawing.Point(160, 41);
+            this.textBoxData.Name = "textBoxData";
+            this.textBoxData.Size = new System.Drawing.Size(208, 31);
+            this.textBoxData.TabIndex = 1;
+            // 
+            // labelKoleksi
+            // 
+            this.labelKoleksi.AutoSize = true;
+            this.labelKoleksi.Location = new System.Drawing.Point(48, 97);
+            this.labelKoleksi.Name = "labelKoleksi";
+            this.labelKoleksi.Size = new System.Drawing.Size(71, 25);
+            this.labelKoleksi.TabIndex = 2;
+            this.labelKoleksi.Text = "Koleksi:";
+            // 
+            // listBoxData
+            // 
+            this.listBoxData.FormattingEnabled = true;
+            this.listBoxData.ItemHeight = 25;
+            this.listBoxData.Location = new System.Drawing.Point(48, 145);
+            this.listBoxData.Name = "listBoxData";
+            this.listBoxData.Size = new System.Drawing.Size(238, 329);
+            this.listBoxData.TabIndex = 3;
+            this.listBoxData.SelectedIndexChanged += new System.EventHandler(this.listBoxData_SelectedIndexChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(427, 44);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(112, 34);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(377, 440);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(348, 34);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // labelSetting
+            // 
+            this.labelSetting.AutoSize = true;
+            this.labelSetting.Location = new System.Drawing.Point(345, 113);
+            this.labelSetting.Name = "labelSetting";
+            this.labelSetting.Size = new System.Drawing.Size(72, 25);
+            this.labelSetting.TabIndex = 6;
+            this.labelSetting.Text = "Setting:";
+            // 
+            // checkBoxAktif
+            // 
+            this.checkBoxAktif.AutoSize = true;
+            this.checkBoxAktif.Location = new System.Drawing.Point(427, 113);
+            this.checkBoxAktif.Name = "checkBoxAktif";
+            this.checkBoxAktif.Size = new System.Drawing.Size(75, 29);
+            this.checkBoxAktif.TabIndex = 7;
+            this.checkBoxAktif.Text = "Aktif";
+            this.checkBoxAktif.UseVisualStyleBackColor = true;
+            this.checkBoxAktif.CheckedChanged += new System.EventHandler(this.checkBoxAktif_CheckedChanged);
+            // 
+            // radioButtonMerah
+            // 
+            this.radioButtonMerah.AutoSize = true;
+            this.radioButtonMerah.Checked = true;
+            this.radioButtonMerah.Location = new System.Drawing.Point(416, 164);
+            this.radioButtonMerah.Name = "radioButtonMerah";
+            this.radioButtonMerah.Size = new System.Drawing.Size(142, 29);
+            this.radioButtonMerah.TabIndex = 8;
+            this.radioButtonMerah.TabStop = true;
+            this.radioButtonMerah.Text = "Warna Merah";
+            this.radioButtonMerah.UseVisualStyleBackColor = true;
+            this.radioButtonMerah.CheckedChanged += new System.EventHandler(this.radioButtonMerah_CheckedChanged);
+            // 
+            // radioButtonBiru
+            // 
+            this.radioButtonBiru.AutoSize = true;
+            this.radioButtonBiru.Location = new System.Drawing.Point(416, 210);
+            this.radioButtonBiru.Name = "radioButtonBiru";
+            this.radioButtonBiru.Size = new System.Drawing.Size(122, 29);
+            this.radioButtonBiru.TabIndex = 9;
+            this.radioButtonBiru.Text = "Warna Biru";
+            this.radioButtonBiru.UseVisualStyleBackColor = true;
+            this.radioButtonBiru.CheckedChanged += new System.EventHandler(this.radioButtonBiru_CheckedChanged);
+            // 
+            // labelOut
+            // 
+            this.labelOut.AutoSize = true;
+            this.labelOut.Location = new System.Drawing.Point(343, 262);
+            this.labelOut.Name = "labelOut";
+            this.labelOut.Size = new System.Drawing.Size(73, 25);
+            this.labelOut.TabIndex = 10;
+            this.labelOut.Text = "Output:";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelOutput.Location = new System.Drawing.Point(453, 328);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 54);
+            this.labelOutput.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 549);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelOut);
+            this.Controls.Add(this.radioButtonBiru);
+            this.Controls.Add(this.radioButtonMerah);
+            this.Controls.Add(this.checkBoxAktif);
+            this.Controls.Add(this.labelSetting);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxData);
+            this.Controls.Add(this.labelKoleksi);
+            this.Controls.Add(this.textBoxData);
+            this.Controls.Add(this.labelData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelData;
+        private System.Windows.Forms.TextBox textBoxData;
+        private System.Windows.Forms.Label labelKoleksi;
+        private System.Windows.Forms.ListBox listBoxData;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelSetting;
+        private System.Windows.Forms.CheckBox checkBoxAktif;
+        private System.Windows.Forms.RadioButton radioButtonMerah;
+        private System.Windows.Forms.RadioButton radioButtonBiru;
+        private System.Windows.Forms.Label labelOut;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ tgsw5/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tgsw5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public DataTable cek = new DataTable();
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            cek.Rows.Add(textBoxData.Text);
+            int sama = 0;
+            for (int i = 0; i < cek.Rows.Count; i++)
+            {
+                if (textBoxData.Text == cek.Rows[i][0].ToString() )
+                {
+                    sama++;
+                }
+            }
+            if(sama == 1)
+            {
+                listBoxData.Items.Add(textBoxData.Text);
+            }
+            else
+            {
+                MessageBox.Show("Data yang dimasukkan tidak boleh sama!");
+            }
+            textBoxData.Text = "";
+            textBoxData.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cek.Columns.Add("data");
+        }
+
+        private void listBoxData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            labelOutput.Text = listBoxData.SelectedItem.ToString();
+        }
+
+        private void checkBoxAktif_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBoxAktif.Checked == true && radioButtonMerah.Checked)
+            {
+                    labelOutput.ForeColor = Color.Red;
+            }
+            else if(checkBoxAktif.Checked == true && radioButtonBiru.Checked)
+            {
+                    labelOutput.ForeColor = Color.Blue;
+            }
+            else
+            {
+                labelOutput.ForeColor = Color.Black;
+            }
+        }
+
+        private void radioButtonMerah_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxAktif.Checked == true && radioButtonMerah.Checked)
+            {
+                labelOutput.ForeColor = Color.Red;
+            }
+        }
+
+        private void radioButtonBiru_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxAktif.Checked == true && radioButtonBiru.Checked)
+            {
+                labelOutput.ForeColor = Color.Blue;
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            listBoxData.Items.Clear();
+            checkBoxAktif.Checked = false;
+            radioButtonMerah.Checked = true;
+            radioButtonBiru.Checked = false;
+            labelOutput.Text = "";
+        }
+    }
+}
